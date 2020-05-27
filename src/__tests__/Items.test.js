5d22b130d18b29c8b6b91e208e30dfeac10e66da
Matt Dignum <matt.dignum@googlemail.com>
2020-05-27 14:36:28 +0100

    port code from old repo to new

---
+++ src/__tests__/Items.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Items from "../components/Items";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({adapter: new Adapter()});
+
+const mockItems = [
+  {children: ["Hello", "World"]},
+  {children: ["How Are", "You"]}
+];
+
+describe("Items", () => {
+  it("renders without crashing", () => {
+    const wrapper = shallow(<Items items={mockItems} />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("matches snapshot", () => {
+    const wrapper = shallow(<Items items={mockItems} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("contains an Item component for each Item", () => {
+    const wrapper = shallow(<Items items={mockItems} />);
+    expect(wrapper.find("Item")).toHaveLength(mockItems.length);
+  });
+
+  it("returns null when there is no data to map", () => {
+    const wrapper = shallow(<Items />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("doesn't break without items", () => {
+    const wrapper = shallow(<Items />);
+    expect(wrapper.find("Item")).toHaveLength(0);
+  });
+
+  it("doesn't break with empty items", () => {
+    const wrapper = shallow(<Items items={[]} />);
+    expect(wrapper.find("Item")).toHaveLength(0);
+  });
+});
---
+++ src/api/fileTypeDetectionApi.js
@@ -0,0 +1,48 @@
+const fileTypeDetectionSuffix = '/api/FileTypeDetection/base64';
+
+const getFileType = (file) => {
+  return readFileBase64Async(file).then(base64 => {
+    var raw = JSON.stringify({ "Base64": base64 });
+    var url = process.env.REACT_APP_FILETYPEDETECTION_API_ENDPOINT + fileTypeDetectionSuffix;
+    return callFileTypeDetection(url, raw);
+  });
+}
+
+const readFileBase64Async = (file) => {
+  return new Promise((resolve, reject) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      var base64 = reader.result.replace(/^data:.+;base64,/, '');
+      resolve(base64);
+    };
+  });
+}
+
+const callFileTypeDetection = (url, raw) => {
+  const promise = new Promise((resolve, reject) => {
+      resolve(fetch(url, {
+        method: 'POST',
+        body: raw,
+        headers: {
+          "Content-Type": "application/json"
+      }})
+      .then ((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+        else{
+          throw new Error('Something went wrong');
+        }
+      })
+      .catch (error => {
+        console.log("Error occured: " + error)
+      }))
+    });
+
+  return promise;
+}
+
+export const fileTypeDetectionApi = {
+  getFileType
+};
