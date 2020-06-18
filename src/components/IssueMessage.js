715a8c0f6856417a986f073c7a71070ea728136f
Matt Dignum <matt.dignum@googlemail.com>
2020-06-18 10:31:06 +0100

    fix typo in issue message

--- src/components/IssueMessage.js
+++ src/components/IssueMessage.js
@@ -5,7 +5,7 @@ class IssueMessage extends React.Component {
 
   render() {
     if (this.props.hasIssues) {
-      return <div className="has-issues"><h1>Unable to protect file due to structual issues</h1></div>
+      return <div className="has-issues"><h1>Unable to protect file due to structural issues</h1></div>
 	}
 	else {
 	return null
