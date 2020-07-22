02ef9426485e508e9003a1cc5bcd46a8236eb9fd
Oleg Taranenko MP <olegtaranenko@gmail.com>
2020-07-23 00:58:55 +0200

    TASK-2545 decouple menu/href from presentation in the footer

--- src/components/sections/header/TopMenu.js
+++ src/components/sections/header/TopMenu.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import logo from '../../../logo.svg';
+import topMenu from '../../../data/topMenu.json';
+import links from '../../../data/links.json';
 
 export default function TopMenu({ toggleMenu, toggleModal } = { }) {
     return (
         <div className="app-menu-top">
-            <div className="app-logo"><a href="https://glasswallsolutions.com/" target="blank" ><img src={logo} alt="Logo" /></a></div>
+            <div className="app-logo"><a href={links.glasswall} target="blank" ><img src={logo} alt="Logo" /></a></div>
             <nav>
                 <button className="app-menu-button" onClick={toggleMenu}></button>
                 {/*<button className="app-menu-info-button" onClick={toggleModal} style={{ display: "none" }}></button>*/}
                 <ul className="app-menu-list">
-                    <li>
-                        <a href="https://engineering.glasswallsolutions.com/docs/products/cloud-sdk/sample-files" target="_blank" rel="noopener noreferrer">
-                            Sample Files
-                        </a>
-                    </li>
-                    <li><a href="https://glasswallsolutions.com/file-drop/">Product Info</a></li>
-                    {/* <li><a href="https://glasswall-store.com/">Visit Store</a></li> */}
-                    <li><a href="https://glasswallsolutions.com/contact">Contact</a></li>
+                    {topMenu.map(item => <li><a href={links[item]}>{item}</a></li>)}
                 </ul>
             </nav>
         </div>
